ffbe353e1406ab6302768e6bddda07682ee99416
VazVel <152049637+VazVel@users.noreply.github.com.>
2024-11-23 13:08:12 -0600

    Pruebas

---
+++ index.js
@@ -0,0 +1,27 @@
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const pool = new Pool({
+  connectionString: "postgres://default:eVWlOCi8a4pE@ep-weathered-dew-a417d6tq-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require",
+});
+
+(async () => {
+  try {
+    // Conexión al pool
+    const client = await pool.connect();
+    console.log('Conexión exitosa a la base de datos.');
+
+    // Consulta para obtener todos los registros de la tabla "cliente"
+    const res = await client.query('SELECT * FROM trabajador;');
+    console.log('Registros de la tabla "cliente":', res.rows);
+
+    // Libera el cliente
+    client.release();
+  } catch (err) {
+    console.error('Error al realizar la consulta:', err.message || err);
+  } finally {
+    // Cierra el pool al finalizar
+    await pool.end();
+    console.log('Pool cerrado.');
+  }
+})();
